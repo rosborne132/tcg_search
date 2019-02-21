bfc18362ccf632634e49ef548fd615bd6fe9e1e8
Robert Osborne <robertosborne@Rob-the-Bob-Osborne-MBP13ntb.local>
2019-02-21 05:47:02 -0800

    populate search options

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
 "use strict";
 
-$.getJSON("pokemon.json", function(json) {
-  const pokemon = json;
-  console.log(pokemon);
-});
+function populateJson(jsonData, location) {
+  $.getJSON(jsonData, function(json) {
+    json.map(data =>
+      $(location).append(`<option value="${data}"> ${data}</option>`)
+    );
+  });
+}
+
+function makeFetch() {
+  // console.log("make fetch");
+  fetch("https://api.pokemontcg.io/v1/sets")
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error(res.statusText);
+    })
+
+    // Main issue
+    .then(resJson => console.log(resJson))
+    .catch(err => {
+      $("#js-error-message").text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function main() {
+  // makeFetch();
+  populateJson("./data/pokemon.json", "#pokemon_names");
+  populateJson("./data/sets.json", "#pokemon_sets");
+  populateJson("./data/types.json", "#pokemon_types");
+  populateJson("./data/subtypes.json", "#pokemon_subtypes");
+}
+
+$(main());
+
+// Hardcoded url to buy pokemon cards
+// https://shop.tcgplayer.com/pokemon/${SET_NAME}?ProductName=${CARD_NAME}
