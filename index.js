ff61d9f8e31d3b99b4751dfcf6f6cfd9719db8d0
Robert Osborne <robertosborne@Rob-the-Bob-Osborne-MBP13ntb.local>
2019-02-21 06:26:22 -0800

    Add onchange handlers

--- index.js
+++ index.js
@@ -8,25 +8,49 @@ function populateJson(jsonData, location) {
   });
 }
 
-function makeFetch() {
+function displayCard(card) {
+  console.log(card);
+}
+
+function displayCards(resData) {
+  resData.cards.map(card => displayCard(card));
+}
+
+function makeFetch(type, params = "") {
   // console.log("make fetch");
-  fetch("https://api.pokemontcg.io/v1/sets")
+  fetch(`https://api.pokemontcg.io/v1/${type}?${params}`)
     .then(res => {
       if (res.ok) {
         return res.json();
       }
       throw new Error(res.statusText);
     })
-
-    // Main issue
-    .then(resJson => console.log(resJson))
+    .then(resJson => displayCards(resJson))
     .catch(err => {
       $("#js-error-message").text(`Something went wrong: ${err.message}`);
     });
 }
 
+$("#pokemon_names").change(function() {
+  const val = $("#pokemon_names").val();
+  console.log(val);
+  makeFetch("cards", `name=${val}`);
+});
+
+$("#pokemon_sets").change(function() {
+  console.log($("#pokemon_sets").val());
+});
+
+$("#pokemon_types").change(function() {
+  console.log($("#pokemon_types").val());
+});
+
+$("#pokemon_subtypes").change(function() {
+  console.log($("#pokemon_subtypes").val());
+});
+
 function main() {
-  // makeFetch();
+  // makeFetch("sets");
   populateJson("./data/pokemon.json", "#pokemon_names");
   populateJson("./data/sets.json", "#pokemon_sets");
   populateJson("./data/types.json", "#pokemon_types");
@@ -37,3 +61,9 @@ $(main());
 
 // Hardcoded url to buy pokemon cards
 // https://shop.tcgplayer.com/pokemon/${SET_NAME}?ProductName=${CARD_NAME}
+
+// My api calls
+// Sets: cards?set=Base
+// Types: cards?types=water
+// Subtypes: cards?subtype=mega
+// Pokemon: cards?name=pikachu
